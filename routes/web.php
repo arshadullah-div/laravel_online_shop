cae5a08ca689429dd759e2a1b5890cbfba43aa4e
arshadullah-div <iamarshadullahpro@gmail.com>
2024-01-25 12:15:10 +0500

    update

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::group(['prefix' => 'admin'],function (){
+
+    Route::group(['middleware' => 'admin.guest'],function (){
+
+        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
+        Route::post('/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+
+    });
+
+    Route::group(['middleware' => 'admin.auth'],function (){
+
+        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
+        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+
+        //Category Routes
+        Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
+        Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
+
+    });
+
+});
